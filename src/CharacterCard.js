e73a03a1d0e39e3e5b3a29e1fe0ca272fb552a39
Theethawat <5910110150@psu.ac.th>
2018-09-20 11:56:22 +0700

    update Character Card

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -18,7 +18,7 @@ export default class CharacterCard extends Component {
     render(){
         let className=`card ${this.state.active ? 'activeCard' : ''} `
         return(
-            <div className={className} onClick={this.activate}>
+            <div className={className} onClick={this.activate                   }>
                 {this.props.value}
             </div>
         )
