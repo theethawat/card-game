eaf14818f157c0782df20acdb27cbd9f3bcda9b7
Theethawat <5910110150@psu.ac.th>
2018-09-20 11:01:26 +0700

    Show Character Card "Hello"

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import logo from './logo.svg';
 import CharacterCard from './CharacterCard';
 import './App.css';
 
+const word = "Hello";
 class App extends Component {
   render() {
     return (
       <div>
-        <CharacterCard value="h"/>
-        <CharacterCard value="i"/>
+      {
+        /*This is JSX */
+        Array.from(word).map((c,i) => <CharacterCard value = {c} key = {i} />)
+      }
       </div>
     );
   }
