e6502ce9d899e555c4a34e0f7c22ebd16329dad4
Theethawat <5910110150@psu.ac.th>
2018-09-23 23:32:43 +0700

    Working-1 Insert Bootstrap 4.1 CSS Library CDN to index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import WordCard from './WordCard';
 import './App.css';
-
 //const word = "Hello";
 class App extends Component {
   render() {
     return (
       <div>
-  
+      <h3>โปรแกรมเดาคำให้ถูกต้อง</h3>
+      {/*เป็นการเรียกใช้ class ที่ชื่อ WordCard โดยส่งคำว่า Hello ไป*/}
       <WordCard value="Hello" />
       </div>
     );
